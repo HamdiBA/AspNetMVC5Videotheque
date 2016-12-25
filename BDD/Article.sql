5f0263b829b5da9c8534b78cc3d8fbc10551266e
Hamdi Benaoun <Hamdi Benaoun>
2016-12-25 14:20:40 +0100

    add BDD

---
+++ BDD/Article.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Article]
+(
+	[ID] INT NOT NULL PRIMARY KEY,
+	[Nom_art] VARCHAR(100) NOT NULL,
+	[Categorie] VARCHAR(100) NOT NULL,
+	[Genre] VARCHAR(100) NOT NULL,
+	[Quantite] INT NOT NULL,
+	[Prix] FLOAT NOT NULL,
+	[Date_ajout] DATETIME DEFAULT(getdate())
+
+)
